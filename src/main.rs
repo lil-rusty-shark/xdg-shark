2356ec97b09705cae9b844b08a1abca9d52a0ad4
lil-rusty-shark <lil.rusty.shark@proton.me>
2023-04-28 09:46:57 +0100

    added basic implementation of cl arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short='v', long, help="display messages for all files checked (verbose)")]
+    skip_ok: bool,
+
+    #[arg(short='q', long, help="doesnt display messages for files without fixes (quite)")]
+    skip_unsupported: bool,
 }
+
+
+fn main () {
+    let cli = Cli::parse();
+}
+
