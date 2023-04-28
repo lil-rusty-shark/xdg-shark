a13ad7730179bb4651bf9a7ead7188131e28615a
lil-rusty-shark <lil.rusty.shark@proton.me>
2023-04-28 09:56:35 +0100

    implemented checking presence of xdg envrioment variable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::env;
 
 
 #[derive(Parser)]
@@ -12,7 +13,54 @@ struct Cli {
 }
 
 
+struct EnvVar {
+    name: &'static str,
+    message: &'static str,
+    recommended_value: &'static str,
+}
+
+
+fn check_envrioment_variables () {
+        let env_vars = [
+        EnvVar {
+            name: "XDG_DATA_HOME",
+            message: "The $XDG_DATA_HOME environment variable is not set, make sure to add it to your shell's configuration before setting any of the other environment variables!",
+            recommended_value: "$HOME/.local/share",
+        },
+        EnvVar {
+            name: "XDG_CONFIG_HOME",
+            message: "The $XDG_CONFIG_HOME environment variable is not set, make sure to add it to your shell's configuration before setting any of the other environment variables!",
+            recommended_value: "$HOME/.config",
+        },
+        EnvVar {
+            name: "XDG_STATE_HOME",
+            message: "The $XDG_STATE_HOME environment variable is not set, make sure to add it to your shell's configuration before setting any of the other environment variables!",
+            recommended_value: "$HOME/.local/state",
+        },
+        EnvVar {
+            name: "XDG_CACHE_HOME",
+            message: "The $XDG_CACHE_HOME environment variable is not set, make sure to add it to your shell's configuration before setting any of the other environment variables!",
+            recommended_value: "$HOME/.cache",
+        },
+        EnvVar {
+            name: "XDG_RUNTIME_DIR",
+            message: "The $XDG_RUNTIME_DIR environment variable is not set, make sure to add it to your shell's configuration before setting any of the other environment variables!",
+            recommended_value: "/run/user/$UID",
+        },
+    ];
+
+    for env_var in &env_vars {
+        if env::var(env_var.name).is_err() {
+            println!("{} {}", env_var.message, env_var.recommended_value);
+        }
+    }
+}
+
+
 fn main () {
     let cli = Cli::parse();
+    check_envrioment_variables()
+
+
 }
 
